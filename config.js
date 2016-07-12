c16905594bc774fcfee8e775666208622d59992e
cbilliau <christopher@billiau.com>
2016-07-11 20:24:44 -0400

    Adjusted config.js to get heroku to work. Possible env variable issue?

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 // Define url variable
 var mlabUrl = 'mongodb://user1:abc123@ds033865.mlab.com:33865/cbilliaudata';
 
-
-
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        process.env.NODE_ENV === 'production' ?
