fd25bed2fc65c740e5fed800eff41a0376709526
cbilliau <christopher@billiau.com>
2016-07-11 21:03:35 -0400

    Continue work on config.js file to have it work on Heroku and Travis. Issue seems to be the setup of the process.env.

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // Define url variable
 var mlabUrl = 'mongodb://user1:abc123@ds033865.mlab.com:33865/cbilliaudata';
 
-exports.DATABASE_URL = mlabUrl;
-// process.env.DATABASE_URL ||
-//                        global.DATABASE_URL ||
-//                        process.env.NODE_ENV === 'production' ?
-//                             'mongodb://localhost/shopping-list' :
-//                             mlabUrl;
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       process.env.NODE_ENV === 'production' ?
+                            'mongodb://localhost/shopping-list' :
+                            mlabUrl;
 exports.PORT = process.env.PORT || 8080;
