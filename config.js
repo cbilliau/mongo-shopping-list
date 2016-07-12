c4260a2e923f6ea452c79dc9166c9d17a36e90a6
cbilliau <christopher@billiau.com>
2016-07-11 20:03:44 -0400

    Updated config.js url to point twds mLab db.

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
+// Define url variable
+var mlabUrl = 'mongodb://user1:abc123@ds033865.mlab.com:33865/cbilliaudata';
+
+
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev';
+                            mlabUrl;
 exports.PORT = process.env.PORT || 8080;
